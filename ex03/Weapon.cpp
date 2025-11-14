3887d3672c416d5b9be72b7ac39f340d640aa58f
Omer Adali <adalomer60@gmail.com>
2025-11-14 04:46:04 +0300

    add ex03

---
+++ ex03/Weapon.cpp
@@ -0,0 +1,25 @@
+#include "Weapon.hpp"
+
+Weapon::Weapon(void)
+{
+	this->_type = "default weapon";
+}
+
+Weapon::Weapon(std::string type)
+{
+	this->_type = type;
+}
+
+Weapon::~Weapon(void)
+{
+}
+
+const std::string&	Weapon::getType(void) const
+{
+	return (this->_type);
+}
+
+void	Weapon::setType(std::string type)
+{
+	this->_type = type;
+}
